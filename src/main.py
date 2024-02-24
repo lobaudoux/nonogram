1a57f04583ee799d519e61feb3f67dfaf34f653a
Louis Baudoux <lba@odoo.com>
2024-02-24 19:05:20 +0100

    Improve execution time

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import argparse
 import time
 
 from gui import GUI
@@ -5,17 +6,30 @@ from nonogram import Nonogram
 
 
 def main():
-    nonogram = Nonogram(n=30, m=30, density=0.65)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', default=25, type=int)
+    parser.add_argument('-m', default=25, type=int)
+    parser.add_argument('-d', '--density', default=0.6, type=float)
+    args = parser.parse_args()
+
+    nonogram = Nonogram(n=args.n, m=args.m, density=args.density)
+
+    density = (
+        (sum(sum(clues) for clues in nonogram.vertical_clues) + sum(sum(clues) for clues in nonogram.horizontal_clues))
+        / (2 * nonogram.x_size * nonogram.y_size)
+    )
+    print(f"Density is {density:.2f}")
 
     gui = GUI(nonogram, draw_crosses=True)
     nonogram.set_gui(gui)
 
-    gui.wait_for_spacebar()
-
     start = time.perf_counter()
+
     nonogram.solve()
+
     print(f"It took {time.perf_counter() - start:.2f}s to solve")
 
+    gui.draw()
     gui.draw_unknown_cells()
 
     gui.wait_for_close()
--- src/nonogram.py
+++ src/nonogram.py
@@ -2,7 +2,7 @@ import bisect
 import itertools
 import random
 import sys
-from collections import deque
+from contextlib import suppress
 
 from constants import *
 
@@ -36,7 +36,7 @@ class Nonogram:
         self.x_size = len(self.vertical_clues)
         self.y_size = len(self.horizontal_clues)
         self.grid = [[UNKNOWN for _ in range(self.y_size)] for _ in range(self.x_size)]
-        self.lines_to_solve = deque()
+        self.lines_to_solve = []
         self.lines_to_solve_set = set()
         self.horizontal_lines = []
         self.vertical_lines = []
@@ -89,11 +89,17 @@ class Nonogram:
         self.gui = gui
         self.gui.draw()
 
+    def _clear_lines_to_solve(self):
+        self.lines_to_solve.clear()
+        self.lines_to_solve_set = set()
+
     def _enqueue_line_to_solve(self, line):
-        self.lines_to_solve.appendleft(line)
-        self.lines_to_solve_set.add(line)
+        if line not in self.lines_to_solve_set:
+            self.lines_to_solve.append(line)
+            self.lines_to_solve_set.add(line)
 
     def _get_next_line_to_solve(self):
+        self.lines_to_solve.sort(key=lambda l: sum(l.clues))
         line = self.lines_to_solve.pop()
         self.lines_to_solve_set.remove(line)
         return line
@@ -119,8 +125,17 @@ class Nonogram:
         clue_index = 0
         cur_placement = []
         while init_cell_indexes[0] < len(values):
-            if (all(init_cell_indexes[clue_index] + i < len(values) and values[init_cell_indexes[clue_index] + i] in {FILLED, UNKNOWN} for i in range(clues[clue_index]))
-                    and (init_cell_indexes[clue_index] + clues[clue_index] >= len(values) or values[init_cell_indexes[clue_index] + clues[clue_index]] in {UNKNOWN, EMPTY})):
+            if (
+                all(
+                    init_cell_indexes[clue_index] + i < len(values)
+                    and values[init_cell_indexes[clue_index] + i] in {FILLED, UNKNOWN}
+                    for i in range(clues[clue_index])
+                )
+                and (
+                    init_cell_indexes[clue_index] + clues[clue_index] >= len(values)
+                    or values[init_cell_indexes[clue_index] + clues[clue_index]] in {UNKNOWN, EMPTY}
+                )
+            ):
                 cur_placement.append((init_cell_indexes[clue_index], clues[clue_index]))
                 if clue_index < len(clues) - 1:
                     init_cell_indexes.append(init_cell_indexes[clue_index] + clues[clue_index] + 1)
@@ -152,78 +167,146 @@ class Nonogram:
 
     @staticmethod
     def optimized_solve_for_values(clues, values):
+        if not any(value == UNKNOWN for value in values):
+            return values
+
+        if sum(1 for value in values if value == FILLED) == sum(clues):
+            return [EMPTY if value == UNKNOWN else value for value in values]
+
+        values_copy = values[:]
+
+        values_start = 0
+        clues_start = 0
+        while (
+            clues_start < len(clues)
+            and all(value == FILLED for value in values_copy[values_start:values_start + clues[clues_start]])
+        ):
+            values_copy[values_start + clues[clues_start]] = EMPTY
+            values_start += clues[clues_start] + 1
+            clues_start += 1
+
+        values_end = len(values_copy)
+        clues_end = len(clues)
+        while (
+            clues_end >= 0
+            and all(value == FILLED for value in values_copy[values_end - clues[clues_end - 1]:values_end])
+        ):
+            values_copy[values_end - clues[clues_end - 1] - 1] = EMPTY
+            values_end -= clues[clues_end - 1] + 1
+            clues_end -= 1
+
+        _values = values_copy[values_start:values_end]
+        _clues = clues[clues_start:clues_end]
+
+        # Solve values based on first filled cells
+        for i, value in enumerate(_values[:_clues[0] + 1]):
+            if value == FILLED:
+                if i == _clues[0]:
+                    _values[0] = EMPTY
+                else:
+                    for j in range(_clues[0] - i):
+                        _values[i + j] = FILLED
+                    if i == 0:
+                        with suppress(IndexError):
+                            _values[_clues[0]] = EMPTY
+                break
+
+        # Solve values based on last filled cells
+        for i, value in enumerate(reversed(_values[-_clues[-1] - 1:])):
+            if value == FILLED:
+                if i == _clues[-1]:
+                    _values[-1] = EMPTY
+                else:
+                    for j in range(_clues[-1] - i):
+                        _values[-i - j - 1] = FILLED
+                    if i == 0:
+                        with suppress(IndexError):
+                            _values[-_clues[-1] - 1] = EMPTY
+                break
+
         # Generate all possible ranges
-        margin = len(values) - sum(clues) - len(clues) + 1
+        margin = len(_values) - sum(_clues) - len(_clues) + 1
         clues_ranges = []
         i = 0
-        for clue in clues:
+        for clue in _clues:
             clues_ranges.append([i + j for j in range(margin + 1)])
             i += clue + 1
 
         # Filter based on empty cells
-        empty_cells = [i for i, value in enumerate(values) if value == EMPTY]
+        empty_cells = [i for i, value in enumerate(_values) if value == EMPTY]
         clues_search_start_i = 0
         for empty_cell_i in empty_cells:
             first_match_found = False
-            for clue_range, clue in zip(clues_ranges[clues_search_start_i:], clues[clues_search_start_i:]):
+            for clue_range, clue in zip(clues_ranges[clues_search_start_i:], _clues[clues_search_start_i:]):
                 if clue_range[0] > empty_cell_i:
                     break
                 elif empty_cell_i < clue_range[-1] + clue:
                     first_match_found = True
                     for i in (i + empty_cell_i - clue + 1 for i in range(clue)):
-                        try:
+                        with suppress(ValueError):
                             clue_range.remove(i)
-                        except ValueError:
-                            pass
                 elif not first_match_found:
                     clues_search_start_i += 1
 
         # Filter based on ranges start
-        for (clue_range, clue), (other_clues_range, other_clue) in zip(zip(clues_ranges[:-1], clues[:-1]), zip(clues_ranges[1:], clues[1:])):
+        for (clue_range, clue), (other_clues_range, other_clue) in zip(
+            zip(clues_ranges[:-1], _clues[:-1]),
+            zip(clues_ranges[1:], _clues[1:]),
+        ):
             other_clues_range[:] = other_clues_range[bisect.bisect_right(other_clues_range, clue_range[0] + clue):]
             clue_range[:] = clue_range[:bisect.bisect_left(clue_range, other_clues_range[-1] - clue)]
 
         # Filter based on filled cells
-        for clue_range, clue in zip(clues_ranges, clues):
+        for clue_range, clue in zip(clues_ranges, _clues):
             values_to_remove = []
             for clue_start_i in clue_range:
-                if (clue_start_i > 0 and values[clue_start_i - 1] == FILLED) or (clue_start_i + clue < len(values) and values[clue_start_i + clue] == FILLED):
+                if (
+                    (clue_start_i > 0 and _values[clue_start_i - 1] == FILLED)
+                    or (clue_start_i + clue < len(_values) and _values[clue_start_i + clue] == FILLED)
+                ):
                     values_to_remove.append(clue_start_i)
             for value in values_to_remove:
                 clue_range.remove(value)
 
-        new_values = [EMPTY if value != FILLED else FILLED for value in values]
-        for clue_range, clue in zip(clues_ranges, clues):
+        new_values = [EMPTY if value != FILLED else FILLED for value in _values]
+        for clue_range, clue in zip(clues_ranges, _clues):
             filled_cells_length = clue_range[0] + clue - clue_range[-1]
             for i in range(clue_range[-1] - clue_range[0] + clue):
-                if values[clue_range[0] + i] == UNKNOWN:
+                if _values[clue_range[0] + i] == UNKNOWN:
                     new_values[clue_range[0] + i] = UNKNOWN
             for i in range(filled_cells_length):
                 new_values[clue_range[0] + clue - filled_cells_length + i] = FILLED
 
-        if sum(clues) == new_values.count(FILLED):
-            return [EMPTY if value == UNKNOWN else value for value in new_values]
+        if sum(_clues) == new_values.count(FILLED):
+            new_values = [EMPTY if value == UNKNOWN else value for value in new_values]
 
-        return new_values
+        _new_values = values_copy[:values_start] + new_values + values_copy[values_end:]
+        if _new_values != values:
+            return _new_values
+        return False
+
+    def is_unsolved(self, line):
+        return any(self.grid[x][y] == UNKNOWN for x, y in line.coordinates)
 
     def _update_grid_from_values(self, line, values):
         for i, (x, y) in enumerate(line.coordinates):
             if values[i] != UNKNOWN and self.grid[x][y] != values[i]:
                 new_line_to_solve = self.horizontal_lines[i] if line.orientation == 'vertical' else self.vertical_lines[i]
-                if new_line_to_solve not in self.lines_to_solve_set and any(self.grid[x][y] == UNKNOWN for x, y in new_line_to_solve.coordinates):
+                if self.is_unsolved(new_line_to_solve):
                     self._enqueue_line_to_solve(new_line_to_solve)
                 self.grid[x][y] = values[i]
+        if self.gui:
+            self.gui.draw()
 
     def solve(self):
         while self.lines_to_solve:
             line = self._get_next_line_to_solve()
             values = self.optimized_solve_for_values(line.clues, [self.grid[x][y] for x, y in line.coordinates])
-            self._update_grid_from_values(line, values)
-            if self.gui:
-                self.gui.draw()
+            if values:
+                self._update_grid_from_values(line, values)
 
         for line in itertools.chain(self.horizontal_lines, self.vertical_lines):
-            if any(self.grid[x][y] == UNKNOWN for x, y in line.coordinates):
+            if self.is_unsolved(line):
                 self._enqueue_line_to_solve(line)
 
         while self.lines_to_solve:
@@ -231,8 +314,7 @@ class Nonogram:
             values = self.solve_for_values(line.clues, [self.grid[x][y] for x, y in line.coordinates])
             if values:
                 self._update_grid_from_values(line, values)
-            if self.gui:
-                self.gui.draw()
+
         return tuple(tuple(self.grid[x][y] for x in range(self.x_size)) for y in range(self.y_size))
 
     def __str__(self):
